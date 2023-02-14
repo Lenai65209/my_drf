d008f61e4a50a7225efca9a04f8bfd4c8ef07cbf
Lenai65209 <lenai65209@rambler.ru>
2023-02-14 19:01:04 +0300

    lesson_12

--- library/library/settings.py
+++ library/library/settings.py
@@ -26,6 +26,13 @@ SECRET_KEY = 'django-insecure-1p2b0ppe*6xezu$2g$mwfp(6y2qgke@=m=cv_nk5da$!r#xmkd
 DEBUG = True
 
 ALLOWED_HOSTS = []
+# ALLOWED_HOSTS = ['*']
+#
+# CORES_ALLOWED_ORIGIN = [
+#     'http://127.0.0.1:3000',
+#     'http://localhost:3000',
+#     'http://0.0.0.0:80',
+# ]
 
 # Application definition
 
@@ -52,8 +59,10 @@ INSTALLED_APPS = [
     'django_filters',
     'rest_framework_simplejwt.token_blacklist',
     'drf_yasg',
-    "graphene_django"
-
+    "graphene_django",
+    # 'django-advanced-filters',
+    'six',
+    'simplejson',
 ]
 
 MIDDLEWARE = [
@@ -73,8 +82,8 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         #  Для проверки работы build
-        # 'DIRS': [BASE_DIR / 'frontend/build'],
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'frontend/build'],
+        # 'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -92,12 +101,23 @@ WSGI_APPLICATION = 'library.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'library',
+            'USER': 'Lena',
+            'PASSWORD': '123456',
+            'HOST': 'db',
+            'PORT': '5432',
+            }
+        }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
@@ -134,7 +154,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 #  Для проверки работы build
-# STATICFILES_DIRS = (BASE_DIR/'frontend/build/static',)
+STATICFILES_DIRS = (BASE_DIR/'frontend/build/static',)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
